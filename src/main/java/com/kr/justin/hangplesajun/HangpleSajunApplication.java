4eb54a535f8d3882f3e46481cfe5d63595a8dc6d
kergosdyr <kergosdyr@kakao.com>
2024-11-08 23:15:19 +0900

    :tada: initial commit

---
+++ src/main/java/com/kr/justin/hangplesajun/HangpleSajunApplication.java
@@ -0,0 +1,13 @@
+package com.kr.justin.hangplesajun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangpleSajunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangpleSajunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kr/justin/hangplesajun/HangpleSajunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.justin.hangplesajun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangpleSajunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
