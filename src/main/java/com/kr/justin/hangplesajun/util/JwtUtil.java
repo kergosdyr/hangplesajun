17bba85fb554d9d1a856fc9fc8d76ca9ead0f70c
kergosdyr <kergosdyr@kakao.com>
2024-11-10 12:51:05 +0900

    feat(security): add jwt, spring security config
    JWT 적용 및 로그인 기능 제공을 위한 spring security 및 JWT 관련 설정 추가

---
+++ src/main/java/com/kr/justin/hangplesajun/util/JwtUtil.java
@@ -0,0 +1,58 @@
+package com.kr.justin.hangplesajun.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.stereotype.Component;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+    private static final String SECRET_KEY = "-rYqHRw35Rvy-5FL3xNZ4gJRuZbTsUPqlWm2jwz1Iio=";
+    private static final long EXPIRATION_TIME = 86400000; // 1일 (밀리초)
+
+    private final SecretKey key;
+
+    public JwtUtil() {
+        this.key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    // 토큰 생성
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuer("hangplesajun")
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    // 토큰에서 사용자명 추출
+    public String getUsernameFromToken(String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    // 토큰 유효성 검사
+    public boolean isValidToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token) {
+        final Date expiration = parseClaims(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
