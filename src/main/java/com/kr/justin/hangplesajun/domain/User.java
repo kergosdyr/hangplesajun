e5d21fff3feaf27ca112d1c15079ed19877712ab
kergosdyr <kergosdyr@kakao.com>
2024-11-21 06:25:15 +0900

    feat(post): eidt User domain
    User domain 수정, table 관련 어노테이션 추가

--- src/main/java/com/kr/justin/hangplesajun/domain/User.java
+++ src/main/java/com/kr/justin/hangplesajun/domain/User.java
@@ -1,10 +1,59 @@
 package com.kr.justin.hangplesajun.domain;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.type.descriptor.java.LocalDateTimeJavaType;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Entity
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "app_user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column
+	private String username;
+
+	@Column
+	private String password;
+
+	@Column
+	private String roles;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime modifiedAt;
+
+	public static User of(
+		String username,
+		String password
+	) {
+		return User.builder()
+			.username(username)
+			.password(password)
+			.roles("USER")
+			.build();
+	}
 
 }
