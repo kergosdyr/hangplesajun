c8b944df14107b8f41021293403f8dd11e20d76b
kergosdyr <kergosdyr@kakao.com>
2024-11-23 22:34:23 +0900

    feat(jpa): add jpa config with auditing config jpa auditing 을 위한 설정 추가

---
+++ src/main/java/com/kr/justin/hangplesajun/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kr.justin.hangplesajun.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
