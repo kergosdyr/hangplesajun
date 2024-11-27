de109230d1b100f076203eb978872dc940142e71
kergosdyr <kergosdyr@kakao.com>
2024-11-27 22:57:50 +0900

    test(post): edit post sql post sql 수정

--- src/test/resources/post_data.sql
+++ src/test/resources/post_data.sql
@@ -1,7 +1,7 @@
 
-DELETE FROM post WHERE id IN (3, 4);
+DELETE FROM post WHERE id IN (10003, 10004);
 
 
 INSERT INTO post (id, user_id, title, content, created_at, modified_at) VALUES
-(10003, 1, 'First Post', 'This is the content of the first post', '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
-(10004, 2, 'Second Post', 'This is the content of the second post', '2023-01-02 00:00:00', '2023-01-02 00:00:00');
+(10003, 10000, 'First Post', 'This is the content of the first post', '2023-01-01 00:00:00', '2023-01-01 00:00:00'),
+(10004, 10001, 'Second Post', 'This is the content of the second post', '2023-01-02 00:00:00', '2023-01-02 00:00:00');
